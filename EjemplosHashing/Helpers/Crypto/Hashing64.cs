b7d010bbe035391a5a2a70b704986ad22f7f6007
Marco Luna <marcoluna@tutanota.com>
2023-11-11 15:55:48 -0600

    Se actualiza hashing

--- EjemplosHashing/Helpers/Crypto/Hashing64.cs
+++ EjemplosHashing/Helpers/Crypto/Hashing64.cs
@@ -31,7 +31,7 @@ namespace EjemplosHashing.Helpers.Crypto
             byte[] inputBytes = Encoding.UTF8.GetBytes(input);
             byte[] hashBytes = sha256.ComputeHash(inputBytes);
 
-            StringBuilder builder = new StringBuilder();
+            StringBuilder builder = new();
             for (int i = 0; i < hashBytes.Length; i++)
             {
                 builder.Append(hashBytes[i].ToString("x2")); // Convierte a formato hexadecimal
